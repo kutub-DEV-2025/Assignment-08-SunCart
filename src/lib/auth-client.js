0ee5106289bc599dd3aa3fa781e4bb6b5b39b1ba
Md. Kutub Uddin <kutub01753@gmail.com>
2026-05-09 10:44:18 +0600

    build

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,4 @@
 
 import { createAuthClient } from "better-auth/react";
 
-
-export const authClient = createAuthClient({
-  baseURL: 'https://assignment-08-sun-cart-gray.vercel.app/',
-});
+export const authClient = createAuthClient();
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,12 +4,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-trustedOrigins: [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://localhost:4173",
-  ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`, `http://${process.env.VERCEL_URL}`] : []),
-],
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
