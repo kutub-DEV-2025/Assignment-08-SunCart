8eee995d16a474fd79bddd5391302b1a7df2d0c3
Md. Kutub Uddin <kutub01753@gmail.com>
2026-05-09 10:28:19 +0600

    origin issue fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,7 +4,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-
+trustedOrigins: [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:4173",
+  ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`, `http://${process.env.VERCEL_URL}`] : []),
+],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
