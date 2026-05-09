671869002dfb8faa880e055be905de64e632db5c
Md. Kutub Uddin <kutub01753@gmail.com>
2026-05-09 10:39:39 +0600

    build

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,6 +2,7 @@
 
 import { createAuthClient } from "better-auth/react";
 
+
 export const authClient = createAuthClient({
-  baseURL: browser ? window.location.origin : undefined,
+  baseURL: 'https://assignment-08-sun-cart-gray.vercel.app/',
 });
